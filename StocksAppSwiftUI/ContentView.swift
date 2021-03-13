60eba60806e6361cf30d0df277c96e4a9df22955
Vinnicius Pereira <pereiradasilva0@hotmail.com>
2021-03-13 16:09:56 -0300

    Initial Commit

---
+++ StocksAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StocksAppSwiftUI
+//
+//  Created by Vinnicius Pereira on 13/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
